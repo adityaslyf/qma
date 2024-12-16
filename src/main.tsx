0359779a4182ebbb55c026e627b42dad74416fce
adityaslyf <aditya.varshneymail@gmail.com>
2024-12-16 23:26:13 +0530

    build fix

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { ClerkProvider } from '@clerk/clerk-react'
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!clerkPubKey) {
-  throw new Error("Missing Clerk Publishable Key")
+  throw new Error("Missking Clerk Publishable Key")
 }
 
 createRoot(document.getElementById('root')!).render(
